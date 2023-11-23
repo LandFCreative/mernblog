cf2681ba18d8d6cee9372b71526fe45800e8b3e8
LandFCreative <deneschukc@yahoo.ca>
2023-11-22 17:43:16 -0700

    adding post :id

--- server/routes/main.js
+++ server/routes/main.js
@@ -36,8 +36,8 @@ router.get('', async (req, res) => {
     } catch (error) {
     console.log(error) 
     }
-
 })
+
 
 router.get('/about', (req, res) => {
     res.render("about")
@@ -46,6 +46,34 @@ router.get('/about', (req, res) => {
 router.get('/contact', (req, res) => {
     res.render("contact")
 })
+
+
+// Routes
+// GET METHOD POST:id
+router.get('/post/:id', async (req, res) => {
+    try {
+      let slug = req.params.id;
+  
+      const data = await Post.findById({ _id: slug });
+  
+      const locals = {
+        title: data.title,
+        description: "Simple Blog created with NodeJs, Express & MongoDb.",
+      }
+  
+      res.render('post', { 
+        locals,
+        data,
+        currentRoute: `/post/${slug}`
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  
+  });
+  
+  
+
 
 
 module.exports= router; 
